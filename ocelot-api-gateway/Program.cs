54647f5f0fd9bd75c9b6a85b5b9466bb7d5fad89
Sjors Scholten <scholtensjors22@outlook.com>
2022-03-28 07:22:08 +0200

    routing config

--- ocelot-api-gateway/Program.cs
+++ ocelot-api-gateway/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Ocelot.DependencyInjection;
@@ -12,6 +14,9 @@ namespace ocelot_api_gateway
     {
         public static void Main(string[] args)
         {
+            //CreateHostBuilder(args).Build().Run();
+
+
             new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
@@ -23,17 +28,59 @@ namespace ocelot_api_gateway
                         .AddJsonFile($"ocelot.{host.HostingEnvironment.EnvironmentName}.json", false, true)
                         .AddEnvironmentVariables();
                 })
-                .ConfigureServices(s =>
-                {
-                    s.AddOcelot();
-                })
-                .UseIISIntegration()
+                .ConfigureLogging(logging => logging.AddConsole())
+                .ConfigureServices(service => service.AddOcelot())
                 .Configure(app =>
                 {
+                    app.UseRouting();
+                    app.UseEndpoints(endpoints => endpoints.MapControllers());
                     app.UseOcelot().Wait();
                 })
-                .Build()
-                .Run();
+                .Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((host, config) =>
+                {
+                    config.AddJsonFile($"ocelot.json", true, true);
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureLogging(logging => logging.AddConsole());
+        }
+
+    }
+
+    public class Startup
+    {
+        public Startup()
+        {
+            
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddOcelot();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseOcelot().Wait();
         }
     }
 }
